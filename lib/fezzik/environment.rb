176d0a6e45337c0051472483ae62650fdb99cc29
Daniel MacDougall <dmacdougall@gmail.com>
2012-02-02 18:39:41 -0800

    Save per-server configs in @environments
    TODO: write a .rake file to output them on the servers

--- lib/fezzik/environment.rb
+++ lib/fezzik/environment.rb
@@ -1,11 +1,16 @@
-# TODO: Support per-server environments
+# TODO: Write .rake file for saving per-server configs
 module Fezzik
-  def self.env(key, value)
-    @environment ||= {}
-    @environment[key] = value
+  def self.env(key, value, opts={})
+    # TODO: Should be able to specify either domain.com or user@domain.com
+    opts[:hosts] = Array(opts[:hosts])
+    options = {
+      :hosts => domain
+    }.merge(opts)
+    @environments ||= Hash.new { |h, k| h[k] = {} }
+    options[:hosts].each { |host| @environments[host][key] = value }
   end
 
-  def self.environment
-    @environment ||= {}
+  def self.environments
+    @environments ||= Hash.new { |h, k| h[k] = {} }
   end
 end
