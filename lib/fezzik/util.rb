284d8d02889f4160bf348b5f644182967cb8d94a
Daniel MacDougall <dmacdougall@gmail.com>
2012-02-08 15:40:09 -0800

    Move Fezzik.capture_output to Fezzik::Util.capture_output

--- lib/fezzik/util.rb
+++ lib/fezzik/util.rb
@@ -1,10 +1,12 @@
 module Fezzik
-  def self.capture_output(&block)
-    output = StringIO.new
-    $stdout = output
-    block.call
-    return output.string
-  ensure
-    $stdout = STDOUT
+  module Util
+    def self.capture_output(&block)
+      output = StringIO.new
+      $stdout = output
+      block.call
+      return output.string
+    ensure
+      $stdout = STDOUT
+    end
   end
 end
