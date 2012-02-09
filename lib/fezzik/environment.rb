af1733dd478520f362c59a65cde56862315964c3
Daniel MacDougall <dmacdougall@gmail.com>
2012-02-08 17:39:01 -0800

    Refactor environment host options

--- lib/fezzik/environment.rb
+++ lib/fezzik/environment.rb
@@ -1,11 +1,11 @@
 # TODO: Write .rake file for saving per-server configs
 module Fezzik
-  def self.env(key, value, opts={})
+  def self.env(key, value, options={})
     # TODO: Should be able to specify either domain.com or user@domain.com
-    opts[:hosts] = Array(opts[:hosts])
     options = {
       :hosts => domain
-    }.merge(opts)
+    }.merge(options)
+    options[:hosts] = Array(options[:hosts])
     @environments ||= Hash.new { |h, k| h[k] = {} }
     options[:hosts].each { |host| @environments[host][key] = value }
   end
