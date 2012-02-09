2cbd8d07bcbb4b4430a0945688a21cda930f65c7
Daniel MacDougall <dmacdougall@gmail.com>
2012-02-08 17:36:26 -0800

    Move split_task_and_params to Fezzik::Util and use it in fez

--- lib/fezzik.rb
+++ lib/fezzik.rb
@@ -75,16 +75,4 @@ require "fezzik/version.rb"
       #applicable_servers.each { |s| @per_server_environments[s][key] = value }
     #end
   #end
-
-  #def split_task_and_params(task_with_params)
-    #params_match = /(.+)\[(.+)\]/.match(task_with_params)
-    #if params_match
-      #task = params_match[1]
-      #params = params_match[2].split(",")
-    #else
-      #task = task_with_params
-      #params = nil
-    #end
-    #[task, params]
-  #end
 #end
--- lib/fezzik/util.rb
+++ lib/fezzik/util.rb
@@ -8,5 +8,17 @@ module Fezzik
     ensure
       $stdout = STDOUT
     end
+
+    def self.split_task_and_params(task_with_params)
+      params_match = /(.+)\[(.+)\]/.match(task_with_params)
+      if params_match
+        task = params_match[1]
+        params = params_match[2].split(",")
+      else
+        task = task_with_params
+        params = nil
+      end
+      [task, params]
+    end
   end
 end
