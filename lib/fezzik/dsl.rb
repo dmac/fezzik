3509ec37d15c2f1098c77a70a96834082ad504b3
Daniel MacDougall <dmacdougall@gmail.com>
2012-05-22 10:19:40 -0700

    Add DSLs for custom 'role' and 'remote_task' methods

--- lib/fezzik/dsl.rb
+++ lib/fezzik/dsl.rb
@@ -1,5 +1,12 @@
 module Fezzik
   module DSL
+    # This is necessary to override Rake::RemoteTasks's globally eval'ed method definitions.
+    def self.included(klass)
+      klass.class_eval do
+        def role(*args) Fezzik.role(*args) end
+        def remote_task(*args, &block) Fezzik.remote_task(*args, &block) end
+      end
+    end
     def destination(name, &block) Fezzik.destination(name, &block) end
     def env(*args) Fezzik.env(*args) end
     def capture_output(&block) Fezzik::Util.capture_output(&block) end
