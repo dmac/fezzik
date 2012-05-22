b8a76ced76c05ddac39e308c1acbce550623d036
Daniel MacDougall <dmacdougall@gmail.com>
2012-05-22 10:18:53 -0700

    Shorten DSL definitions

--- lib/fezzik/dsl.rb
+++ lib/fezzik/dsl.rb
@@ -1,15 +1,7 @@
 module Fezzik
   module DSL
-    def destination(name, &block)
-      Fezzik.destination(name, &block)
-    end
-
-    def env(*args)
-      Fezzik.env(*args)
-    end
-
-    def capture_output(&block)
-      Fezzik::Util.capture_output(&block)
-    end
+    def destination(name, &block) Fezzik.destination(name, &block) end
+    def env(*args) Fezzik.env(*args) end
+    def capture_output(&block) Fezzik::Util.capture_output(&block) end
   end
 end
