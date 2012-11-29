d84c71755c36efe1c80acc4ebab607ce1e71ada9
Daniel MacDougall <dmacdougall@gmail.com>
2012-11-29 15:45:32 -0800

    Use $stderr for non-critical output to be script-friendly

--- lib/fezzik/base.rb
+++ lib/fezzik/base.rb
@@ -6,7 +6,7 @@ module Fezzik
     @options = options
     @target_destination = ENV["fezzik_destination"].to_sym rescue nil
     unless options[:tasks].nil?
-      puts "Loading Fezzik tasks from #{@options[:tasks]}"
+      $stderr.puts "Loading Fezzik tasks from #{@options[:tasks]}"
       Dir[File.join(Dir.pwd, "#{@options[:tasks]}/**/*.rake")].sort.each { |lib| import lib }
     end
   end
