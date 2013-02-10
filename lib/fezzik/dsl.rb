dbdd836c85949fbc5cd2f739ab703af0cda663bf
Daniel MacDougall <dmacdougall@gmail.com>
2013-02-09 17:45:19 -0800

    Weave: Clean role DSL declaration

--- lib/fezzik/dsl.rb
+++ lib/fezzik/dsl.rb
@@ -1,11 +1,5 @@
 module Fezzik
   module DSL
-    # This is necessary to override Rake::RemoteTask's globally eval'ed method definitions.
-    def self.included(klass)
-      klass.class_eval do
-        def role(*args) Fezzik.role(*args) end
-      end
-    end
     def destination(*names, &block) Fezzik.destination(*names, &block) end
     def env(*args) Fezzik.env(*args) end
     def capture_output(&block) Fezzik::Util.capture_output(&block) end
@@ -13,5 +7,6 @@ module Fezzik
     def get(name) Fezzik.get(name) end
     def remote_task(*args, &block) Fezzik.remote_task(*args, &block) end
     def host_task(name, options = {}, &block) Fezzik.host_task(name, options, &block) end
+    def role(*args) Fezzik.role(*args) end
   end
 end
